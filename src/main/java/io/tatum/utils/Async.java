2188202f0baa7723588e9ba0f4305dc0f3132d86
tiennv <tiennv>
2020-12-10 17:07:38 +0700

    put method

--- src/main/java/io/tatum/utils/Async.java
+++ src/main/java/io/tatum/utils/Async.java
@@ -27,6 +27,24 @@ public class Async {
                 .thenApply(HttpResponse::body).get();
     }
 
+    public static String put(String uri, String apiKey, String requestBody) throws ExecutionException, InterruptedException {
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .timeout(Duration.ofMinutes(1))
+                .header("Content-Type", "application/json")
+                .headers("x-api-key", apiKey)
+                .PUT(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        var client = HttpClient.newHttpClient();
+        return client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(response -> {
+                    System.out.println(response.statusCode());
+                    return response;
+                })
+                .thenApply(HttpResponse::body).get();
+    }
+
     public static String get(String uri, String apiKey) throws ExecutionException, InterruptedException {
         var request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
