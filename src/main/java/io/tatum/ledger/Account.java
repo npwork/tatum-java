6e73ca52b15afe12c8214ec70cb4d36807d88f3f
tiennv <tiennv>
2020-12-10 17:45:55 +0700

    fixed API url

--- src/main/java/io/tatum/ledger/Account.java
+++ src/main/java/io/tatum/ledger/Account.java
@@ -21,7 +21,8 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/getAccountByAccountId" target="_blank">Tatum API documentation</a>
      */
     public io.tatum.model.response.ledger.Account getAccountById(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account" + id : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account" + id;
         String account = Async.get(uri, Env.getTatumApiKey());
         // TO-DO
         return new io.tatum.model.response.ledger.Account();
@@ -33,7 +34,8 @@ public class Account {
     public io.tatum.model.response.ledger.Account createAccount(CreateAccount account) throws IOException, ExecutionException, InterruptedException {
 //        await validateOrReject(account);
 //        TO-DO
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiKey()) ? TATUM_API_URL + "/v3/ledger/account" : Env.getTatumApiKey();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account";
 
         var objectMapper = new ObjectMapper();
         String requestBody = objectMapper.writeValueAsString(account);
@@ -49,8 +51,8 @@ public class Account {
     public io.tatum.model.response.ledger.Account[] createAccounts(CreateAccountsBatch accounts) throws IOException, ExecutionException, InterruptedException {
 //        await validateOrReject(accounts);
 //        TO-DO
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiKey()) ? TATUM_API_URL + "/v3/ledger/account/batch" : Env.getTatumApiKey();
-
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/batch";
         var objectMapper = new ObjectMapper();
         String requestBody = objectMapper.writeValueAsString(accounts);
 
@@ -63,7 +65,8 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/getBlockAmount" target="_blank">Tatum API documentation</a>
      */
     public Blockage[] getBlockedAmountsByAccountId(String id, Integer pageSize, Integer offset) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/block/" + id + "?pageSize=" + pageSize + "&offset=" + offset : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/block/" + id + "?pageSize=" + pageSize + "&offset=" + offset;
         String tx = Async.get(uri, Env.getTatumApiKey());
         // TO-DO
         return new Blockage[]{};
@@ -74,7 +77,8 @@ public class Account {
      */
     public String blockAmount(String id, BlockAmount block) throws IOException, ExecutionException, InterruptedException {
 //        await validateOrReject(block);
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/block/" + id : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/block/" + id;
         var objectMapper = new ObjectMapper();
         String requestBody = objectMapper.writeValueAsString(block);
 
@@ -86,7 +90,8 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/deleteBlockAmount" target="_blank">Tatum API documentation</a>
      */
     public void deleteBlockedAmount(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/block/" + id : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/block/" + id;
         Async.delete(uri, Env.getTatumApiKey());
     }
 
@@ -94,7 +99,8 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/deleteAllBlockAmount" target="_blank">Tatum API documentation</a>
      */
     public void deleteBlockedAmountForAccount(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/block/acoount/" + id : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/block/acoount/" + id;
         Async.delete(uri, Env.getTatumApiKey());
     }
 
@@ -102,7 +108,8 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/activateAccount" target="_blank">Tatum API documentation</a>
      */
     public void activateAccount(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/" + id + "/activate" : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/" + id + "/activate";
         String requestBody = "{}";
         Async.put(uri, Env.getTatumApiKey(), requestBody);
     }
@@ -111,18 +118,18 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/deactivateAccount" target="_blank">Tatum API documentation</a>
      */
     public void deactivateAccount(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/" + id + "/deactivate" : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/" + id + "/deactivate";
         String requestBody = "{}";
         Async.put(uri, Env.getTatumApiKey(), requestBody);
     }
-
-    ;
 
     /**
      * For more details, see <a href="https://tatum.io/apidoc#operation/freezeAccount" target="_blank">Tatum API documentation</a>
      */
     public void freezeAccount(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/" + id + "/freeze" : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/" + id + "/freeze";
         String requestBody = "{}";
         Async.put(uri, Env.getTatumApiKey(), requestBody);
     }
@@ -131,18 +138,18 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/unfreezeAccount" target="_blank">Tatum API documentation</a>
      */
     public void unfreezeAccount(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/" + id + "/unfreeze" : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/" + id + "/unfreeze";
         String requestBody = "{}";
         Async.put(uri, Env.getTatumApiKey(), requestBody);
     }
-
-    ;
 
     /**
      * For more details, see <a href="https://tatum.io/apidoc#operation/getAccountsByCustomerId" target="_blank">Tatum API documentation</a>
      */
     public io.tatum.model.response.ledger.Account[] getAccountsByCustomerId(String id, Integer pageSize, Integer offset) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/customer/" + id + "?pageSize=" + pageSize + "&offset=" + offset : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/customer/" + id + "?pageSize=" + pageSize + "&offset=" + offset;
         String accounts = Async.get(uri, Env.getTatumApiKey());
         // TO-DO
         return new io.tatum.model.response.ledger.Account[]{};
@@ -152,7 +159,8 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/getAllAccounts" target="_blank">Tatum API documentation</a>
      */
     public io.tatum.model.response.ledger.Account[] getAllAccounts(Integer pageSize, Integer offset) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account?pageSize=" + pageSize + "&offset=" + offset : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account?pageSize=" + pageSize + "&offset=" + offset;
         String accounts = Async.get(uri, Env.getTatumApiKey());
         // TO-DO
         return new io.tatum.model.response.ledger.Account[]{};
@@ -162,7 +170,8 @@ public class Account {
      * For more details, see <a href="https://tatum.io/apidoc#operation/getAccountBalance" target="_blank">Tatum API documentation</a>
      */
     public AccountBalance getAccountBalance(String id) throws IOException, ExecutionException, InterruptedException {
-        String uri = Strings.isNullOrEmpty(Env.getTatumApiUrl()) ? TATUM_API_URL + "/v3/ledger/account/" + id + "/balance" : Env.getTatumApiUrl();
+        String tatumApiUrl = Env.getTatumApiUrl();
+        String uri = (Strings.isNullOrEmpty(tatumApiUrl) ? TATUM_API_URL : tatumApiUrl) + "/v3/ledger/account/" + id + "/balance";
         String accounts = Async.get(uri, Env.getTatumApiKey());
         // TO-DO
         return new AccountBalance();
