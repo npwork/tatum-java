39908a30097809808388c1a217c7c7a1ed687d8d
nvxtien <nvxtien@gmail.com>
2020-12-21 17:23:33 +0700

    fix errors (#17)
    * delete kotlin
    
    * define API-based methods for blockchains
    
    * delete method
    
    * put method
    
    * APi-based methods of ledger account
    
    * fixed API url
    
    * remove explicitly importing
    
    * change class to LedgerAccount
    
    * 20 seconds timeout
    
    * change ledger account name
    
    * fix bcash
    
    * ledger customer
    
    * order book
    
    * add Record
    
    * ledger Subscription
    
    * ledger Transaction
    
    * lombok compile
    
    * API-bases methods for KMS
    
    * lombok compile
    
    * applied lombok
    
    * add details for blockchain API, test code
    
    * update ApiKey and BaseUrl.java
    
    * update API details and test code
    
    * update code
    
    * update details for API
    
    * update details for API
    
    * add details to APIs and test code
    
    * remove unused import package
    
    * Ledger account test
    
    * test create account for the ledger
    
    * test create account for the ledger
    
    * unit test ledger account
    
    * unit test customer ledger
    
    * unit test customer ledger
    
    * unit test order book
    
    * unit test
    
    * unit test
    
    * clean code
    
    * validate objects
    
    * validate objects
    
    * validate CreateAccount
    
    * validate CreateAccount
    
    * validate object and unit test
    
    * clean code
    
    * transaction for bitcoin and litecoin
    
    * apply async
    
    * apply async
    
    * change names
    
    * sign KMS transaction
    
    * bcach transaction
    
    * fix litecoin
    
    * fix errors
    
    Co-authored-by: tiennv <tien.nguyenv@enjoyworks.co.kr>
    Co-authored-by: tiennv <tiennv>

--- src/test/java/io/tatum/transaction/BitcoinTxTest.java
+++ src/test/java/io/tatum/transaction/BitcoinTxTest.java
@@ -1,86 +1,65 @@
 package io.tatum.transaction;
 
-import io.tatum.blockchain.Bcash;
 import io.tatum.model.request.transaction.FromUTXO;
 import io.tatum.model.request.transaction.To;
-import io.tatum.model.request.transaction.TransferBchBlockchain;
-import io.tatum.model.response.bch.BchTx;
-import io.tatum.transaction.bcash.TransactionBuilder;
-import io.tatum.utils.ObjectValidator;
-import io.tatum.utils.Promise;
-import org.apache.commons.lang3.ArrayUtils;
-import org.bitcoinj.core.NetworkParameters;
+import io.tatum.model.request.transaction.TransferBtcBasedBlockchain;
+import org.bitcoinj.core.Address;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.LegacyAddress;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
+import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Stream;
 
-public class BcashTx {
+import static io.tatum.constants.Constant.BITCOIN_MAINNET;
+import static io.tatum.constants.Constant.BITCOIN_TESTNET;
+import static org.junit.Assert.assertEquals;
 
-    /**
-     * Sign Bitcoin Cash transaction with private keys locally. Nothing is broadcast to the blockchain.
-     *
-     * @param network mainnet or testnet version
-     * @param body    content of the transaction to broadcast
-     * @returns transaction data to be broadcast to blockchain.
-     */
-    public String prepareBitcoinCashSignedTransaction(NetworkParameters network, TransferBchBlockchain body) throws ExecutionException, InterruptedException {
-        if (!ObjectValidator.isValidated(body)) {
-            return null;
-        }
+public class BitcoinTxTest {
 
-        return CompletableFuture.supplyAsync(() -> {
-            try {
-                FromUTXO[] fromUTXO = body.getFromUTXO();
-                To[] to = body.getTo();
-
-                var transactionBuilder = new TransactionBuilder(network);
-                for (var item : to) {
-                    transactionBuilder.addOutput(item.getAddress(), item.getValue());
-                }
-
-                String[] txHashs = Stream.of(fromUTXO).map(u -> u.getTxHash()).toArray(size -> new String[size]);
-                List<BchTx> txs = getTransactions(txHashs);
-
-
-                for (int i = 0; i < fromUTXO.length; i++) {
-                    FromUTXO item = fromUTXO[i];
-                    BigDecimal value = new BigDecimal(txs.get(i).getVout()[item.getIndex().intValue()].getValue());
-                    value.setScale(8, RoundingMode.FLOOR);
-                    long satoshis = value.multiply(BigDecimal.valueOf(100000000)).longValue();
-                    transactionBuilder.addInput(item.getTxHash(), item.getIndex().longValue(), item.getPrivateKey(), satoshis);
-                }
-
-                return transactionBuilder.build().toHex();
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-                return null;
-            }
-        }).get();
-
+    @Test
+    public void testP2SHOutputScript() {
+        String P2SHAddressString = "35b9vsyH1KoFT5a5KtrKusaCcPLkiSo1tU";
+        Address P2SHAddress = LegacyAddress.fromBase58(BITCOIN_MAINNET, P2SHAddressString);
+        Script script = ScriptBuilder.createOutputScript(P2SHAddress);
+        Transaction tx = new Transaction(BITCOIN_MAINNET);
+        tx.addOutput(Coin.COIN, script);
+        assertEquals(P2SHAddressString, tx.getOutput(0).getScriptPubKey().getToAddress(BITCOIN_MAINNET).toString());
     }
 
-    public List<BchTx> getTransactions(String[] txHash) throws ExecutionException, InterruptedException {
-        List<CompletableFuture<BchTx>> futures = new ArrayList<>();
-        if (ArrayUtils.isEmpty(txHash)) {
-            Bcash bcash = new Bcash();
-            CompletableFuture future;
-            for (String tx : txHash) {
-                future = CompletableFuture.supplyAsync(() -> {
-                    try {
-                        return bcash.bcashGetTransaction(tx);
-                    } catch (ExecutionException | InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    return null;
-                });
-                futures.add(future);
-            }
-        }
-        return Promise.all(futures).get();
+    @Test
+    public void prepareSignedTransactionTest() throws ExecutionException, InterruptedException {
+
+        var body = new TransferBtcBasedBlockchain();
+        FromUTXO[] fromUTXO = new FromUTXO[1];
+        fromUTXO[0] = new FromUTXO("53faa103e8217e1520f5149a4e8c84aeb58e55bdab11164a95e69a8ca50f8fcc",
+                BigDecimal.valueOf(0),
+                "cVX7YtgL5muLTPncHFhP95oitV1mqUUA5VeSn8HeCRJbPqipzobf");
+        body.setFromUTXO(fromUTXO);
+
+        To[] to = new To[1];
+        to[0] = new To("2MzNGwuKvMEvKMQogtgzSqJcH2UW3Tc5oc7", new BigDecimal(0.02969944));
+        body.setTo(to);
+
+        BitcoinTx tx = new BitcoinTx();
+
+        var txData = tx.prepareSignedTransaction(BITCOIN_TESTNET, body);
+        assertEquals("02000000" + // version
+                "01" + // num txIn
+                "cc8f0fa58c9ae6954a1611abbd558eb5ae848c4e9a14f520157e21e803a1fa53" + // txHash
+                "00000000" +
+                "6a4730440220" +
+                "5e49848369acc41719b669dcc9ba486c570f1ca4974f61a4321329fe35e3ff36022007485588ede47e17db992ba41aef35c72cb292f9889d471f2c592fb7f252672e0" +
+                "12103b17a162956975765aa6951f6349f9ab5bf510584c5df9f6065924bfd94a08513" +
+                "ffffffff" +
+                "01" + // num txOut
+                "58512d0000000000" +
+                "17a914" +
+                "4e1e4321307c88ecd4ddd6aeec040c6f01e53c99" + // to address
+                "87" +
+                "00000000", txData);
     }
 }
