7beac0bf4b192f088b2f35b69f1d6bab4714bc86
tiennv <nvxtien@gmail.com>
2021-01-06 10:42:06 +0700

    fixed errors

--- src/main/java/io/tatum/model/request/TransferXlmOffchain.java
+++ src/main/java/io/tatum/model/request/TransferXlmOffchain.java
@@ -5,37 +5,20 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-/**
- * The type Transfer eth offchain.
- */
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @EqualsAndHashCode
-public class TransferEthOffchain {
+public class TransferXlmOffchain {
 
-    @Size(min=1, max=500)
     @NotEmpty
-    private String mnemonic;
-
-    @Min(0)
-    @NotEmpty
-    private int index;
-
-    @Size(min=66, max=66)
-    @NotEmpty
-    private String privateKey;
-
-    @Size(max=50000)
-    private String data;
+    @Size(min=56, max=56)
+    private String secret;
 
     @NotNull
-    @Valid
-    private BaseTransferEthErc20Offchain baseTransferEthErc20Offchain;
+    private CreateWithdrawal withdrawal;
 }
